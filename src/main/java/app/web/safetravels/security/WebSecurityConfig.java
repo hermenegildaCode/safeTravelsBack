8e63919a7141b698b0f576dc22eb40d30092624c
Monica Veiga Puente <monicaveiga@protonmail.com>
2022-01-06 22:26:56 +0100

    prepping add for heroku - changed ctxt-path

--- src/main/java/app/web/safetravels/security/WebSecurityConfig.java
+++ src/main/java/app/web/safetravels/security/WebSecurityConfig.java
@@ -53,6 +53,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.authorizeRequests()
+			.antMatchers("/api/**").permitAll()
 			.antMatchers("/api/auth/**").permitAll()
 			.antMatchers("/api/auth/login").permitAll()
 			.antMatchers("/api/test/**").permitAll()
